a8b61664a0ca45f03877d88ea2f057116fc0f01e
Caroline Forget <caro.forget23@gmail.com>
2020-11-27 09:53:32 -0500

    Debut du JS pour aller chercher de l'information dans la base de donnees MovieDB.

---
+++ src/js/script.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", function (){
+
+    let connexion = new MovieDB();
+    connexion.requeteDernierFilm();
+
+
+
+
+
+})
+
+class MovieDB{
+    constructor() {
+        console.log("MovieDB");
+        this.APIkey = "1c4178447548302fa472b09818a8d46e";
+        this.lang = "fr-CA";
+        this.baseURL = "https://api.themoviedb.org/3";
+        this.imgPath = "https://image.tmdb.org/t/p/";
+        this.totalFilm = 8;
+    }
+
+    requeteDernierFilm() {
+        let requete = new XMLHttpRequest();
+
+        requete.addEventListener("loadend", this.retourRequeteDernierFilm.bind(this));
+        //requete.open("GET","https://api.themoviedb.org/3/movie/now_playing?api_key=1c4178447548302fa472b09818a8d46e&language=fr-CA&page=1");
+        requete.open("GET",this.baseURL + "/movie/now_playing?api_key=" + this.APIkey + "&language=" + this.lang + "&page=1");
+        requete.send();
+
+    }
+
+    retourRequeteDernierFilm(e) {
+        console.log("retour dernier film");
+
+        let target = e.currentTarget;
+        let data;
+
+        //console.log(target.responseText);
+
+        data = JSON.parse(target.responseText).results;
+
+        console.log(data);
+
+        this.afficheDernierFilm(data);
+    }
+
+    afficheDernierFilm(data){
+        for (let i = 0; i < data.length; i++) {
+            console.log(data[i].title);
+            console.log(data[i].overview);
+        }
+    }
+
+
+}
